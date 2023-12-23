e4db625ea0c46083c0c427b2c5d2318d1f77a423
dolevi <domi.genz@gmx.de>
2023-12-23 22:08:20 +0100

    fix cells with two word starts

--- index.js
+++ index.js
@@ -10,18 +10,17 @@ const content = csvParse.parse(fs.readFileSync(SOURCE_FILE), {bom: true, delimit
 
 const {positionObjArr, width, height} = cwg.default(content.map(c => c[0]));
 
-const crossword = Array.from({length: width}, () => Array.from({length: height}, () => ({occupied: false, wordNum: []})));
+const crossword = Array.from({length: width}, () => Array.from({length: height}, () => ({occupied: false, wordNum: {}})));
 for (const word of positionObjArr) {
-    crossword[word.xNum][word.yNum].wordNum.push(content.findIndex(elem => elem[0] === word.wordStr));
     if (word.isHorizon) {
+        crossword[word.xNum][word.yNum].wordNum.horizontal = content.findIndex(elem => elem[0] === word.wordStr) + 1;
         for (let x = word.xNum; x < word.xNum + word.wordStr.length; x++) {
             crossword[x][word.yNum].occupied = true;
-            crossword[x][word.yNum].wordStr = word.wordStr;
         }
     } else {
+        crossword[word.xNum][word.yNum].wordNum.vertical = content.findIndex(elem => elem[0] === word.wordStr) + 1;
         for (let y = word.yNum; y < word.yNum + word.wordStr.length; y++) {
             crossword[word.xNum][y].occupied = true;
-            crossword[word.xNum][y].wordStr = word.wordStr;
         }
     }
 }
@@ -62,13 +61,14 @@ let html = `<html><style>${css}</style><body><table style="border-spacing: 0;">`
 for (const row of crossword) {
     html += "<tr>";
     for (const cell of row) {
-        if (cell.wordNum.length === 1) {
-            html += `<td class="cell letter">${cell.wordNum[0] + 1}</td>`;
-        } else if (cell.wordNum.length === 2) {
-            const numElements = `<div class="cell left">${cell.wordNum[0] + 1}</div><div class="cell upper">${cell.wordNum[1] + 1}</div>`;
+        if (cell.wordNum.horizontal !== undefined && cell.wordNum.vertical !== undefined) {
+            const numElements = `<div class="cell left">${cell.wordNum.vertical}</div><div class="cell upper">${cell.wordNum.horizontal}</div>`;
             html += `<td class="cell letter relative">${numElements}</td>`;
+        } else if (cell.wordNum.horizontal !== undefined) {
+            html += `<td class="cell letter">${cell.wordNum.horizontal}</td>`;
+        } else if (cell.wordNum.vertical !== undefined) {
+            html += `<td class="cell letter">${cell.wordNum.vertical}</td>`;
         } else if (cell.occupied) {
-            // html += `<td>${cell.letter}</td>`;
             html += `<td class="cell letter"></td>`;
         } else {
             html += `<td class="cell"></td>`;
@@ -80,6 +80,6 @@ for (const row of crossword) {
 html += "</table>" +
     "<div style='display: flex; width: fit-content;'>" + hintsTables.join("") + "</div>" +
     "</body></html>";
-console.log(`Processed ${hints.length} words`)
+console.log(`Processed ${hints.length} words`);
 
 fs.writeFileSync(DESTINATION_FILE, html);
